59e494db4a93a18637ef62e329a6f3affc7e3878
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2021-10-07 07:52:12 +0200

    first

---
+++ w1/mavendemo/src/main/java/refl/example/Main.java
@@ -0,0 +1,15 @@
+package refl.example;
+
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+import static org.reflections.scanners.Scanners.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
